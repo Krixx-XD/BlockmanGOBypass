32206ddcb68ec369bfa5934a68c0f136e488e12b
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-10-21 21:40:59 +0500

    Create ezz.lua

---
+++ ezz.lua
@@ -0,0 +1,2 @@
+--Protected with /????\
+do local A = not A while A==not A do A=not A end end for a,J in pairs({})do (function(a)a={"#E#EE#e3ee3e#3ee#e3e3e#e3e3eeee3e3e3#EE"}end)()end;do local H = 9 if(-H==-1)then (function(aaAAaaaA)end)() end
